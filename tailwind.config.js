b54a93fe19742948d4a1102172ea06464bcdef2d
SanaanAzfar <sanaanazfar742004@gmail.com>
2026-06-07 11:12:00 +0500

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./**/*.{html,razor,cshtml}",
+    "./Components/**/*.{razor,html,cshtml}",
+    "./Pages/**/*.{razor,html,cshtml}",
+    "./Shared/**/*.{razor,html,cshtml}",
+    "./wwwroot/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
