4e7c2e1d4ae8097899a3f31b0ded03b1f438b5cd
SanaanAzfar <sanaanazfar742004@gmail.com>
2026-07-23 23:05:35 +0500

    Profile in progress Thu Jul 23 11:05:35 PM PKT 2026

--- obj/Debug/net8.0/Profolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Profolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Profolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfa043d578ac6ac0aad5b59e52c8e994d5a6deb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f3467e807a2760ea8848aef2e7b12788a26887")]
 [assembly: System.Reflection.AssemblyProductAttribute("Profolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Profolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
